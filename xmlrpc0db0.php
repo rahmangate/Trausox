723748688d309b2f99e09f4a97ab70f05800129e
rahman <mohammedabdulrahman81@gmail.com>
2019-12-20 07:01:14 -1200

    trausox website

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>http://applauz.bold-themes.com/main-demo</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="http://applauz.bold-themes.com/main-demo/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://applauz.bold-themes.com/main-demo/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://applauz.bold-themes.com/main-demo/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="http://applauz.bold-themes.com/main-demo/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="http://applauz.bold-themes.com/main-demo/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
